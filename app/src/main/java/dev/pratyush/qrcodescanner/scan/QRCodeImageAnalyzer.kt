050ea3a3fc8d8158fef3db65affac208c520fa49
Pratyush <codelab@pratyush.dev>
2021-07-14 21:21:01 +0530

    added support for all types of Qr code

--- app/src/main/java/dev/pratyush/qrcodescanner/scan/QRCodeImageAnalyzer.kt
+++ app/src/main/java/dev/pratyush/qrcodescanner/scan/QRCodeImageAnalyzer.kt
@@ -4,10 +4,38 @@ import android.graphics.ImageFormat
 import androidx.camera.core.ImageAnalysis.Analyzer
 import androidx.camera.core.ImageProxy
 import com.google.zxing.*
+import com.google.zxing.BarcodeFormat.*
 import com.google.zxing.common.HybridBinarizer
-import com.google.zxing.multi.qrcode.QRCodeMultiReader
+import java.util.*
 
-class QRCodeImageAnalyzer( private val listener: (qrCode: String?) -> Unit) : Analyzer {
+
+class QRCodeImageAnalyzer(
+    private var hints : List<BarcodeFormat> = allSupportedFormat,
+    private val listener: (qrCode: String?) -> Unit
+    ) : Analyzer {
+
+    companion object{
+        val allSupportedFormat = listOf(
+            AZTEC,
+            CODABAR,
+            CODE_39,
+            CODE_93,
+            CODE_128,
+            DATA_MATRIX,
+            EAN_8,
+            EAN_13,
+            ITF,
+            MAXICODE,
+            PDF_417,
+            QR_CODE,
+            RSS_14,
+            RSS_EXPANDED,
+            UPC_A,
+            UPC_E,
+            UPC_EAN_EXTENSION
+        )
+
+    }
 
     override fun analyze(image: ImageProxy) {
 
@@ -28,7 +56,14 @@ class QRCodeImageAnalyzer( private val listener: (qrCode: String?) -> Unit) : An
             )
             val binaryBitmap = BinaryBitmap(HybridBinarizer(source))
             try {
-                val result = QRCodeMultiReader().decode(binaryBitmap)
+                val supportedHints: MutableMap<DecodeHintType, Any> = EnumMap(
+                    DecodeHintType::class.java
+                )
+                supportedHints[DecodeHintType.POSSIBLE_FORMATS] = hints
+                val result = MultiFormatReader().decode(
+                    binaryBitmap,
+                    supportedHints
+                )
                 listener.invoke(result.text)
             } catch (e: FormatException) {
                 e.fillInStackTrace()
